841059fffc2d159b4d2929acc9fc62bed34e2b0f
colaliu <colaliu@tencent.com>
2021-04-15 21:10:26 +0800

    test: add math test

--- src/math/math.service.spec.ts
+++ src/math/math.service.spec.ts
@@ -15,4 +15,9 @@ describe('MathService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it ('should be return correct count', () => {
+    expect(service.calculateWordCount('a b c c')).toEqual({ a: 1, b: 1, c: 2})
+    expect(service.calculateWordCount('c c c d')).toEqual({ c: 3, d: 1})
+  })
 });
